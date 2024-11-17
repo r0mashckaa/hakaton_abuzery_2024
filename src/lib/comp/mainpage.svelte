5328dbc834573bcca6259f04aafcfc25b41bceb8
r0mashcka <145837752+r0mashckaa@users.noreply.github.com>
2024-11-17 05:48:27 +0500

    site

---
+++ src/lib/comp/mainpage.svelte
@@ -0,0 +1,82 @@
+<div class="all">
+    <img
+        src="https://avatars.mds.yandex.net/i?id=8796c790f304c8ffaf899e5bbbd88563_l-7452498-images-thumbs&n=13"
+        alt=""
+        class="mainimg"
+    />
+    <h1>Бот для сбора взносов для профсоюза СурГУ</h1>
+    <div class="text">
+        <h4>Команда "Абузеры", совместно с профсоюзом Сургутского Государственного</h4>
+        <h4>Университета, представляет вам удобного в использовании и безопасного</h4>
+        <h4>telegram-бота, с помощью которого можно облегчить и автоматизировать</h4>
+        <h4>сбор взносов для студентов, обучающихся на внебюджетные или временно</h4>
+        <h4>не получающих стипендию, а также минимизировать участие профоргов.</h4>
+        <h4 class="boturl"><a href="https://t.me/vznos_surgu_bot"><img src="/tg.png" alt="" class="tg" /></a> - ссылка на нашего бот</h4>
+    </div>
+</div>
+
+<style>
+    .all {
+        padding: 10px;
+        justify-content: center;
+        border: 2px blue;
+        border-top-right-radius: 4px rgb(0, 77, 0);
+        border-radius: 60px;
+        background: linear-gradient(50deg, blue, rgb(0, 83, 0));
+    }
+    @font-face {
+        font-family: "Ruthles-Sketch";
+        src: local("Ruthless-Sketch"),
+            url(/Ruthless-Sketch/RuthlessSketch-Italic.ttf);
+    }
+    h1 {
+        margin-top: 0;
+        margin-bottom: 0;
+        color: white;
+        font-family: "Ruthles-Sketch";
+    }
+
+    h4 {
+        justify-content: center;
+        color: black;
+        font-family: "cursive";
+        font-size: 20px;
+        padding: 0;
+        margin: 3px;
+        align-self: center;
+    }
+    .boturl {
+        margin-left: 150px;
+        margin-top: 10px
+    }
+    .mainimg {
+        margin: 2px, 2px, 2px, 0;
+        align-self: center;
+        width: 700px;
+        border-top-left-radius: 52px;
+        border-top-right-radius: 52px;
+        border-bottom-right-radius: 10px;
+        border-bottom-left-radius: 10px;
+    }
+    .text {
+        background: white;
+        margin-bottom: 0;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        border-bottom-right-radius: 52px;
+        border-bottom-left-radius: 52px;
+        padding-top: 8px;
+        padding-bottom: 15px;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+    }
+    .tg {
+        width: 60px;
+        transition: all 0.6s;
+    }
+    .tg:hover {
+        transform: scale(1.2);
+        filter: drop-shadow(0 0 10px #4893e8);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,75 @@
+<script>
+    import Mainpage from "$lib/comp/mainpage.svelte";
+</script>
+
+<div class="all">
+    <a href="https://www.surgu.ru/index"><img src="\surgulogo.png" alt="" class="surlog" /></a>
+    <a href="https://t.me/s/profsouzSurGU"><img src="\prophsoyuse.png" alt="" class="soyuselog" /></a>
+    <a href="https://web.telegram.org/a/"><img src="\support.png" alt="" class="support" /></a>
+    <img src="/tgbot.jpg" alt="" class="tgbot" />
+    <div class="page">
+        <Mainpage />
+    </div>
+</div>
+
+<style>
+    .page {
+        padding-top: 35px;
+        display: grid;
+        justify-content: center;
+        align-content: center;
+    }
+    .surlog {
+        display: block;
+        position: fixed;
+        width: 230px;
+        margin-top: 320px;
+        margin-left: 75px;
+        rotate: 345deg;
+        transition: all 0.6s;
+    }
+    .surlog:hover {
+        transform: scale(1.4);
+        filter: drop-shadow(0 0 20px #0d5ab3);
+    }
+    .soyuselog {
+        display: block;
+        position: fixed;
+        width: 230px;
+        margin-top: 100px;
+        margin-left: 1175px;
+        rotate: 15deg;
+        transition: all 0.6s;
+    }
+    .soyuselog:hover {
+        transform: scale(1.4);
+        filter: drop-shadow(0 0 20px #1a92b7);
+    }
+    .support {
+        display: block;
+        position: fixed;
+        width: 140px;
+        margin-top: 79px;
+        margin-left: 58px;
+        rotate: 349deg;
+        transition: all 0.6s;
+    }
+    .support:hover {
+        transform: scale(1.4);
+        filter: drop-shadow(0 0 20px #049bcd);
+    }
+    .tgbot {
+        display: block;
+        position: fixed;
+        width: 200px;
+        margin-top: 440px;
+        margin-left: 1200px;
+        transition: all 0.6s;
+    }
+    .tgbot:hover {
+        transform: scale(1.3);
+    }
+    .all {
+        background: linear-gradient(45deg, white, rgb(222, 214, 214));
+    }
+</style>
